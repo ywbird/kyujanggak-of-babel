47829ef3540a38191c91facc5b7408e3e3f165f8
ywbird <ywbird@naver.com>
2026-03-03 14:00:53 +0900

    Moved getRandomArray from index.js to utils.js

--- utils.js
+++ utils.js
@@ -278,3 +278,16 @@ function charI2B(num) {
   } 
   return null;
 }
+
+
+// 
+
+function getRandomArray(n, min, max) {
+  const array = new Uint32Array(n); // 32비트 정수 사용 (더 정밀한 범위 분배 가능)
+  window.crypto.getRandomValues(array);
+
+  // 32비트 최대값(0xFFFFFFFF)으로 나누어 0~1 사이 값으로 변환 후 범위 적용
+  return Array.from(array, (num) => {
+    return Math.floor((num / 0xFFFFFFFF) * (max - min + 1)) + min;
+  });
+}
